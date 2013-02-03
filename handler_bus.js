2e6ef90cdf1ab11ebc8893f77787952243af1df2
Andrew Li <andrewli92@gmail.com>
2013-02-03 02:06:26 +0000

    Rewrote bus parser with cleaner code and makes use of new modules

---
+++ handler_bus.js
@@ -0,0 +1,112 @@
+/*
+ *  This module queries TFL's bus API with the given parameters,
+ *  parses the response, manipulates it to remove useless information
+ *  and then returns it to the client.
+ */
+
+var http = require("http-get");
+var fs = require("fs");
+var moment = require("moment");
+var serve = require("./serve");
+
+var dataloc;
+
+function start(param, response) {
+    if(param === undefined) { serve.error(response, 416); }
+    get(response, param);
+}
+
+function get(response, param) {
+    var regex = /\?[Ss]top[Cc]ode1\=[0-9][0-9][0-9][0-9][0-9]/;
+    if(!regex.test(param)) { serve.error(response, 416); }
+    
+    var tflurl = { url: "http://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1" };
+    var returnlist = "ReturnList=DestinationText,LineName,EstimatedTime";
+    tflurl.url = tflurl.url + param + "&" + returnlist;
+    var rand = Math.floor(Math.random() * 90000);
+    dataloc = "./data/bus" + rand + ".txt";
+    
+    http.get(tflurl, dataloc, function (error, result) {
+        if(error) { serve.error(response, 416); }
+        else {
+            //console.log("File downloaded at " + result.file);
+            parse(result.file, response);
+        }
+    });
+}
+
+function parse(file, response) {
+    fs.readFile(file, function(error, data) {
+        if(error) { serve.error(response, 500); }
+        var array = data.toString().split("\n");
+        for(var i = 0; i < array.length; i++) {
+            var temp = array[i];
+            array[i] = [];
+            array[i] = temp.toString().split(",");
+        }
+        
+        array = array.sort(compare);
+        manipulateArray(array);
+        var json = createJSON(array);
+        serve.jsonobj(response, json);
+        
+        fs.unlink(dataloc, function(error) {
+            if (error)
+                console.log("Failed to delete " + dataloc);
+        });
+    });
+}
+
+/*  Takes the array and manipulates it to get rid of unwanted data and convert
+    Epoch time into a time counter from "now" (e.g. 'in 1 minute') */
+function manipulateArray(array) {
+    for(var i = 0; i < array.length; i++) {
+        array[i].shift(); //gets rid of the first item
+        array[i][0] = array[i][0].substr(1,array[i][0].length-2); //gets rid of quotation marks for first item
+        if (array[i][2] === undefined) {
+            array[i][1] = array[i][1].substr(0,13); // gets rid of \r at end
+            array[i][1] = moment.utc(parseInt(array[i][1], 10));
+        } else {
+            array[i][1] = array[i][1].substr(1,array[i][1].length-2); // gets rid of quotation marks for destination
+            array[i][2] = array[i][2].substr(0,10); // gets rid of \r at end
+            array[i][2] = moment.unix(array[i][2]).fromNow();
+            if (array[i][2] === "in a few seconds") // Replaces "in a few seconds" with a more suitable phrase
+                array[i][2] = "momentarily";
+        }
+    }
+}
+
+/*  Criteria to sort array. Allows sorting of a 2D array based on the Epoch time stamp.
+    The lower the number, the higher it appears up in the array when the sort is done. */
+function compare(arrayA, arrayB) {
+     return (arrayA[3] == arrayB[3] ? 0 : (arrayA[3] < arrayB[3] ? -1 : 1));
+}
+
+/* Creates a JSON object from the array */
+function createJSON(array) {
+    var json = {
+        "$": {
+            "lastUpdate": "",
+            "version": ""
+        }
+    };
+    for (var i = 0; i < array.length; i++) {
+        if (array[i][2] === undefined) {
+            json.$.lastUpdate = array[i][1];
+            json.$.version = array[i][0];
+        } else {
+            var temp = {
+                "Route": "",
+                "Destination": "",
+                "Time": ""
+            };
+            temp.Route = array[i][0];
+            temp.Destination = array[i][1];
+            temp.Time = array[i][2];
+            json[i] = temp;
+        }
+    }
+    return json;
+}
+
+exports.start = start;
