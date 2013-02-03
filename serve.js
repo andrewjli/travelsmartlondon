942f3350308ed136dd079ab34d3217160847b08b
Andrew Li <andrewli92@gmail.com>
2013-02-03 02:06:10 +0000

    Added module to serve responses

---
+++ serve.js
@@ -0,0 +1,50 @@
+/*
+ *  This module serves a web/error page to the client.
+ */
+
+var fs = require("fs");
+
+function error(response, code) {
+    response.writeHead(code, { "Content-Type": "text/html" });
+    if(code === 404) {
+        fs.readFile('./html/error404.html', function (error, html) {
+            if (error) { console.log(error); }
+            response.write(html);
+            response.end();
+        });
+    }
+    if(code === 416) {
+        fs.readFile('./html/error416.html', function (error, html) {
+            if (error) { console.log(error); }
+            response.write(html);
+            response.end();
+        });
+    }
+	if(code === 500) {
+        fs.readFile('./html/error500.html', function (error, html) {
+            if (error) { console.log(error); }
+            response.write(html);
+            response.end();
+        });
+    }
+}
+
+function webpage(response, page) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    var filename = "./html/" + page + ".html";
+    fs.readFile(filename, function (error, html) {
+        if (error) { console.log(error); }
+        response.write(html);
+        response.end();
+    });
+}
+
+function jsonobj(response, json) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify(json));
+    response.end();
+}
+
+exports.error = error;
+exports.webpage = webpage;
+exports.jsonobj = jsonobj;
