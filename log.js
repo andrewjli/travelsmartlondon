9366adb54d04c9c952409a4455a96121b23a3515
Andrew Li <andrewli92@gmail.com>
2013-03-01 14:20:52 +0000

    Added logging module

---
+++ log.js
@@ -0,0 +1,28 @@
+var winston = require('winston');
+
+function getDate() {
+    var d = new Date();
+    function pad(n) {
+            return n<10 ? '0'+n : n;
+    }
+    return "["+d.getUTCFullYear()+'-'
+            +pad(d.getUTCMonth()+1)+'-'
+            +pad(d.getUTCDate())+' '
+            +pad(d.getUTCHours())+':'
+            +pad(d.getUTCMinutes())+':'
+            +pad(d.getUTCSeconds())+']';
+}
+
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)({ json: false, timestamp: function () { return getDate() }}),
+        new winston.transports.File({ filename: __dirname + '/debug.log', json: false, timestamp: function () { return getDate() }}),
+    ],
+    exceptionHandlers: [
+        new (winston.transports.Console)({ json: false, timestamp: function () { return getDate() }}),
+        new winston.transports.File({ filename: __dirname + '/exceptions.log', json: false, timestamp: function () { return getDate() }}),
+    ],
+    exitOnError: false
+});
+
+module.exports = logger;
