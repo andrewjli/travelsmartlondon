7615ccf039a13794bdadfcab6b77a96bcbe6a41b
Andrew Li <andrewli92@gmail.com>
2013-03-02 14:38:41 +0000

    Updated to meet coding conventions

--- handler_stops.js
+++ handler_stops.js
@@ -1,16 +1,29 @@
-/*
- *  This module queries TFL's bus API with the co-ordinates and radius,
- *  parses the response, manipulates it to remove useless information
- *  and then returns the list of bus stops within a radius of the co-ordinates.
+/**
+ * This module queries TFL's bus API with the co-ordinates and radius,
+ * parses the response, manipulates it to remove useless information
+ * and then returns the list of bus stops within a radius of the co-ordinates.
+ * 
+ * @author  Andrew Li
+ * @version 1.0
  */
 
+/* Required modules */
 var http = require("http-get");
 var fs = require("fs");
 var serve = require("./serve");
 var log = require("./log");
 
+/* Global variables */
 var dataloc;
 
+/**
+ * Checks to see if the query matches the specified
+ * format. If it does, queries the URL downloads the data,
+ * otherwise returns an error
+ * 
+ * @param response the response object created by the server when the request was received
+ * @param param    the client requested parameters
+ */
 function start(response, param) {
     var regex = /\?[Ll]oc\=-?[0-9]+.[0-9]+,-?[0-9]+.[0-9]+,[0-9][0-9][0-9]+/;
     if(regex.test(param)) {
@@ -32,9 +45,18 @@ function start(response, param) {
     }
 }
 
+/**
+ * Reads the downloaded data, turns it into a JSON Object
+ * and sends it as a response to the client request
+ * 
+ * @param file     the downloaded data file location
+ * @param response the response object created by the server when the request was received
+ */
 function parse(file, response) {
     fs.readFile(file, function(error, data) {
-        if(error) { serve.error(response, 500); }
+        if(error) {
+            serve.error(response, 500);
+        }
         var array = data.toString().split("\n");
         for(var i = 0; i < array.length; i++) {
             var temp = array[i];
@@ -46,43 +68,55 @@ function parse(file, response) {
         var json = createJSON(array);
         serve.jsonobj(response, json);
         
+        /* Delete downloaded data */
         fs.unlink(dataloc, function(error) {
-            if (error)
+            if(error) {
                 log.error("Failed to delete " + dataloc);
+            }
         });
     });
 }
 
-/*  Takes the array and filters out useless information */
+/**
+ * Manipulates the array of data and removes useless information
+ * 
+ * @param array an array given by the parse method
+ */
 function manipulateArray(array) {
-    //filter values based on StopPointType and StopCode1
+    /* Filter values based on StopPointType and StopCode1 */
     for(var j = 0; j < array.length; j++) {
         if(array[j][2] === "null" || array[j][3] === "null") {
             array.splice(j,1);
             j--;
         }
     }
-    array.shift(); //removes first row (last updated date)
+    array.shift();                                                      /* Removes first row (last updated date) */
     for(var i = 0; i < array.length; i++) {
-        array[i].shift(); //gets rid of the first item (the 0s)
-        array[i][0] = array[i][0].substr(1,array[i][0].length-2); //gets rid of quotation marks for stop name
-        array[i][1] = array[i][1].substr(1,array[i][1].length-2); //gets rid of quotation marks for stop code
-        array[i][2] = array[i][2].substr(1,array[i][2].length-2); //gets rid of quotation marks for stop type
+        array[i].shift();                                               /* gets rid of the first item */
+        array[i][0] = array[i][0].substr(1,array[i][0].length-2);       /* gets rid of quotation marks for stop name */
+        array[i][1] = array[i][1].substr(1,array[i][1].length-2);       /* gets rid of quotation marks for stop code */
+        array[i][2] = array[i][2].substr(1,array[i][2].length-2);       /* gets rid of quotation marks for stop type */
         if(array[i][3] !== "null") {
-            array[i][3] = array[i][3].substr(1,array[i][3].length-2); //gets rid of quotation marks for stop indicator
+            array[i][3] = array[i][3].substr(1,array[i][3].length-2);   /* gets rid of quotation marks for stop indicator */
         } else {
             array[i][3] = "N/A";
         }
         if(i === array.length-1) {
-            array[i][5] = array[i][5].substr(0,array[i][5].length-1); // gets rid of ] at end
+            array[i][5] = array[i][5].substr(0,array[i][5].length-1);   /* gets rid of ] at end */
         } else {
-            array[i][5] = array[i][5].substr(0,array[i][5].length-2); // gets rid of ]\r at end
+            array[i][5] = array[i][5].substr(0,array[i][5].length-2);   /* gets rid of ]\r at end */
         }
     }
 }
 
-/* Creates a JSON object and populates it with the contents of the array */
-function createJSON(array) {
+/**
+ * Creates a new JSON object and populates it with the
+ * contents of the given data
+ * 
+ * @param data a JSON object returned by the parse method
+ * @return     a JSON object with all the useless data removed
+ */
+function createJSON(data) {
     var json = {
         "stations": []
     };
@@ -94,14 +128,15 @@ function createJSON(array) {
             "long": "",
             "lat": ""
         };
-        temp.stopName = array[i][0];
-        temp.stopCode = array[i][1];
-        temp.stopIndicator = array[i][3];
-        temp.lat = array[i][4];
-        temp.long = array[i][5];
+        temp.stopName = data[i][0];
+        temp.stopCode = data[i][1];
+        temp.stopIndicator = data[i][3];
+        temp.lat = data[i][4];
+        temp.long = data[i][5];
         json.Stations.push(temp);
     }
     return json;
 }
 
+/* Makes start method available to other modules */
 exports.start = start;
--- handler_tube.js
+++ handler_tube.js
@@ -1,9 +1,13 @@
-/*
- *  This module queries TFL's Tube API with the given parameters,
- *  parses the response, manipulates it to remove useless information
- *  and then returns it to the client.
+/**
+ * This module queries TFL's Tube API with the given parameters,
+ * parses the response, manipulates it to remove useless information
+ * and then returns it to the client.
+ * 
+ * @author  Andrew Li
+ * @version 1.0
  */
 
+/* Required modules */
 var http = require("http-get");
 var xml2js = require("xml2js");
 var fs = require("fs");
@@ -11,8 +15,18 @@ var parser = new xml2js.Parser();
 var serve = require("./serve");
 var log = require("./log");
 
+/* Global variables */
 var dataloc;
 
+/**
+ * Checks to see if the query matches the specified
+ * format. If it does, queries the URL downloads the data,
+ * otherwise returns an error. Reads the downloaded data,
+ * turns it into a JSON Object and sends it to the client
+ * 
+ * @param response the response object created by the server when the request was received
+ * @param param    the client requested parameters
+ */
 function start(response, param) {
     var regex = /\?[Ss]top\=[BCDHJMNPVW],[A-Z][A-Z][A-Z]/;
     var paramarray = [];
@@ -26,23 +40,24 @@ function start(response, param) {
         
         tflurl.url = tflurl.url + paramarray[0] + "/" + paramarray[1];
         http.get(tflurl, dataloc, function (error, result) {
-            if (!error) {
+            if(!error) {
                 parser.on("end", function(result) {
-                    //TODO: maniplate json
-                    serve.jsonobj(response, result);
+                    //json = createJSON(result);                            /* TO DO */
+                    serve.jsonobj(response, result /* json */);
                     
+                    /* Delete downloaded data */
                     fs.unlink(dataloc, function(error) {
-                        if (error) {
+                        if(error) {
                             log.error("Failed to delete " + dataloc);
                         }
                     });
                 });
                 
                 fs.readFile(result.file, function(error, data) {
-                    if (error) {
+                    if(error) {
                         log.error(error);
                     }
-                    if (data.toString("hex",0,3) === "efbbbf") {
+                    if(data.toString("hex",0,3) === "efbbbf") {
                         data = data.slice(3); // removes byte order mark
                     }
                     parser.parseString(data);
@@ -54,4 +69,19 @@ function start(response, param) {
     }
 }
 
+/**
+ * Creates a new JSON object and populates it with the useful
+ * contents of the given data
+ * 
+ * @param data an array given by the start method
+ * @return     a JSON object with all the useless data removed
+ */
+function createJSON(data) {
+    json = {
+
+    }
+    return json;
+}
+
+/* Make start method available to other modules */
 exports.start = start;
