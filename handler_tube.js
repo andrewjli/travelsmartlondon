7e0686328827bc75d18169ea49a3d311dd570cd8
Andrew Li <andrewli92@gmail.com>
2013-03-02 11:24:57 +0000

    Restructured and simplified with new query URL

--- handler_tube.js
+++ handler_tube.js
@@ -14,53 +14,44 @@ var log = require("./log");
 var dataloc;
 
 function start(response, param) {
-    var LineFirst = /\?[Ll]ine\=[BCDHJMNPVW]\&[Ss]top\=[A-Z][A-Z][A-Z]/;
-    var StationFirst = /\?[Ss]top\=[A-Z][A-Z][A-Z]\&[Ll]ine\=[BCDHJMNPVW]/;
+    var regex = /\?[Ss]top\=[BCDHJMNPVW],[A-Z][A-Z][A-Z]/;
     var paramarray = [];
-    if(LineFirst.test(param)) {
-        var tarray = param.toString().split("&");
-        tarray[0] = tarray[0].toString().split("=");
-        paramarray[0] = tarray[0][1];
-        tarray[1] = tarray[1].toString().split("=");
-        paramarray[1] = tarray[1][1];
-    } else if(StationFirst.test(param)) {
-        var tarray = param.toString().split("&");
-        tarray[0] = tarray[0].toString().split("=");
-        paramarray[1] = tarray[0][1];
-        tarray[1] = tarray[1].toString().split("=");
-        paramarray[0] = tarray[1][1];
+    if(regex.test(param)) {
+        var tarray = param.toString().split("=");
+        paramarray = tarray[1].toString().split(",");
+
+        var tflurl = { url: "http://cloud.tfl.gov.uk/TrackerNet/PredictionDetailed/" };    
+        var rand = Math.floor(Math.random() * 90000);
+        dataloc = "./data/tube" + rand + ".xml";
+        
+        tflurl.url = tflurl.url + paramarray[0] + "/" + paramarray[1];
+        http.get(tflurl, dataloc, function (error, result) {
+            if (!error) {
+                parser.on("end", function(result) {
+                    //TODO: maniplate json
+                    serve.jsonobj(response, result);
+                    
+                    fs.unlink(dataloc, function(error) {
+                        if (error) {
+                            log.error("Failed to delete " + dataloc);
+                        }
+                    });
+                });
+                
+                fs.readFile(result.file, function(error, data) {
+                    if (error) {
+                        log.error(error);
+                    }
+                    if (data.toString("hex",0,3) === "efbbbf") {
+                        data = data.slice(3); // removes byte order mark
+                    }
+                    parser.parseString(data);
+                });
+            }
+        });
     } else {
         serve.error(response, 416);
     }
-    var tflurl = { url: "http://cloud.tfl.gov.uk/TrackerNet/PredictionDetailed/" };    
-    var rand = Math.floor(Math.random() * 90000);
-    dataloc = "./data/tube" + rand + ".xml";
-    
-    tflurl.url = tflurl.url + paramarray[0] + "/" + paramarray[1];
-    http.get(tflurl, dataloc, function (error, result) {
-        if (!error) {
-            parser.on("end", function(result) {
-                //TODO: maniplate json
-                serve.jsonobj(response, result);
-                
-                fs.unlink(dataloc, function(error) {
-                    if (error) {
-                        log.error("Failed to delete " + dataloc);
-                    }
-                });
-            });
-            
-            fs.readFile(result.file, function(error, data) {
-                if (error) {
-                    log.error(error);
-                }
-                if (data.toString("hex",0,3) === "efbbbf") {
-                    data = data.slice(3); // removes byte order mark
-                }
-                parser.parseString(data);
-            });
-        }
-    });
 }
 
 exports.start = start;
