583830ccff753939da004f6b6509a4681beb7522
Andrew Li <andrewli92@gmail.com>
2013-03-02 12:43:56 +0000

    Updated to meet coding conventions

--- log.js
+++ log.js
@@ -1,5 +1,18 @@
+/**
+ * This module is creates a logger that can be used
+ * to log information, errors and exceptions
+ * 
+ * @author  Andrew Li
+ * @version 1.0
+ */
+
+/* Required modules */
 var winston = require('winston');
 
+/**
+ * Gets the current date and time and returns it
+ * in the desired timestamp format
+ */
 function getDate() {
     var d = new Date();
     function pad(n) {
@@ -13,6 +26,10 @@ function getDate() {
             +pad(d.getUTCSeconds())+']';
 }
 
+/*
+ * Create a new custom logger that prints logs
+ * to Console and to specified files.
+ */
 var logger = new winston.Logger({
     transports: [
         new winston.transports.Console({ json: false, timestamp: function () { return getDate() }}),
@@ -25,4 +42,5 @@ var logger = new winston.Logger({
     exitOnError: false
 });
 
+/* Make logger available to other modules */
 module.exports = logger;
