2d94788321bb0068ae1a18a1d2ea106edbd339a7
Andrew Li <andrewli92@gmail.com>
2013-03-02 20:38:37 +0000

    New timer module to regularly refresh data

---
+++ timer.js
@@ -0,0 +1,24 @@
+/**
+ * This module runs a timer and executes specified
+ * modules at the specified intervals
+ * 
+ * @author  Kamil Przekwas, Andrew Li
+ * @version 1.0
+ */
+
+/* Required modules */
+var lines = require("./updater_lines");
+var bikes = require("./updater_bike");
+
+/**
+ * Starts the timer
+ */
+function start() {
+    /* Lines refresh every 2 minutes */
+    setInterval(lines.start, 120000);
+    /* Bikes refresh every 5 minutes */
+    setInterval(bike.start, 300000);
+}
+
+/* Make method available to other modules */
+exports.start = start;
