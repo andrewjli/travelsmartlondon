b6cabeef607ad170bc8db97af4fec58a4d59db28
Andrew Li <andrewli92@gmail.com>
2013-03-02 12:37:59 +0000

    Updated to meet coding conventions

--- router.js
+++ router.js
@@ -1,15 +1,25 @@
-/*
- *  This module routes the requests that are received
- *  and calls the appropriate request handler based on
- *  the request URL.
+/**
+ * This module routes the requests that are received
+ * and calls the appropriate request handler based on
+ * the request URL
+ * 
+ * @author  Andrew Li
+ * @version 1.0
  */
 
+/* Required modules */
 var serve = require("./serve");
 var log = require("./log");
 
+/**
+ * Checks to see if there is a handler for the
+ * received request. If yes, executes handler.
+ * If no, serves an error
+ * 
+ * @param response the response object created by the server when the request was received
+ * @param code     the HTTP error code to be returned to the user
+ */
 function route(handle, response, pathname, param) {
-    /*  Check if a defined handle exists for the queried path (e.g. /bus)
-        If yes, send handle the request. If not, return 404 */
     if(typeof handle[pathname] === "function") {
         handle[pathname](response, param);
     } else {
@@ -17,4 +27,5 @@ function route(handle, response, pathname, param) {
     }
 }
 
+/* Make method available to other modules */
 exports.route = route;
