5459c3328d754bd7057e25cbcf07dadee39e57fc
Andrew Li <andrewli92@gmail.com>
2013-03-03 00:27:55 +0000

    Fixed typo in variables

--- timer.js
+++ timer.js
@@ -15,13 +15,13 @@ var bikes = require("./updater_bike");
  */
 function start() {
 	/* Updates lines and bikes on start */
-	line.start();
-	bike.start();
+	lines.start();
+	bikes.start();
 
     /* Lines refresh every 2 minutes */
     setInterval(lines.start, 120000);
     /* Bikes refresh every 5 minutes */
-    setInterval(bike.start, 300000);
+    setInterval(bikes.start, 300000);
 }
 
 /* Make method available to other modules */
