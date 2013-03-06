839be4938ce9757d894993de9aa4bd6dc6638067
Andrew Li <andrewli92@gmail.com>
2013-03-06 14:56:13 +0000

    New implementation for db

--- db.js
+++ db.js
@@ -1,41 +1,26 @@
 /**
- * This module queries TFL's Line Status API, parses the response,
- * manipulates it to remove useless information and stores it.
+ * This module opens a connection to the database
  * 
  * @author  Andrew Li
  * @version 1.0
  */
 
- /* Required modules */
-var mongodb = require("mongodb");
-var server = new mongodb.Server("localhost", 27017, { auto_reconnect: true });
-var db = new mongodb.Db("tslDb", server, {w: 1});;
+/* Required modules */
+var log = require("./log");
+var lines = require("./updater_lines");
+var bikes = require("./updater_bike");
+var bikedb = require("mongojs").connect("tslDb", ["bike"]);
+var linedb = require("mongojs").connect("tslDb", ["line"]);
 
 /**
- * Opens the database
- * 
- * @param callback callback function taking (error, db)
+ * Starts the update timer
  */
-function openDatabase(callback) {
-	db.open(function(err, db) {
-		if (err) {
-			return callback(err);
-		}
-	    return callback(null, db);
-	});
-}
-
-/**
- * Connects to the database
- * 
- * @param callback callback function taking (error, collection)
- */
-function connect(db, coll, callback) {
-	var collection = mongodb.Collection(db, coll);
-
-	return callback(null, collection);
+function start() {
+    bikes.start(bikedb);
+    setInterval(function() {
+    	bikes.start(bikedb)
+    }, 30000)
 }
 
 /* Make methods available to other modules */
-exports.openDatabase = openDatabase;
-exports.connect = connect;
+exports.start = start;
