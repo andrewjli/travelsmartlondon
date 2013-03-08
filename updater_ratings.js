60ff776430109a5232cdcaa32c44eb0148109eed
OpenShift guest <kamus4to@gmail.com>
2013-03-08 08:47:16 -0500

    Added functionality of storing individual user ratings in userRatings collection

--- updater_ratings.js
+++ updater_ratings.js
@@ -8,15 +8,15 @@
 /* Required modules */
 var serve = require("./serve");
 var log = require("./log");
-var db = require("mongojs").connect("tslDb", ["ratings"]);
+var db = require("mongojs").connect("tslDb", ["ratings", "userRatings"]);
 
 function start(response, param) {
     var regex = /\?(Piccadilly|District|Victoria|Circle|Hammersmith_and_City|Bakerloo|Waterloo_and_City|Central|Jubilee|Metropolitan|Northern|DLR|Overground)=([0-5])/;
-    var regexForUser = /\?[Ff]or[Uu]ser=^(,).*,(Piccadilly|District|Victoria|Circle|Hammersmith_and_City|Bakerloo|Waterloo_and_City|Central|Jubilee|Metropolitan|Northern|DLR|Overground)=([0-5])/;
-    if(regex.test(param)) {
+    var regexForUser = /\?[Ff]or[Uu]ser=([0-9]|[A-Z]|[a-z])*,(Piccadilly|District|Victoria|Circle|Hammersmith_and_City|Bakerloo|Waterloo_and_City|Central|Jubilee|Metropolitan|Northern|DLR|Overground)=([0-5])/;
+    if(regexForUser.test(param)) {
         getResult(response, param);
-    } else if(regexForUser.test(param)) {
-        console.log("Success!!!");
+    } else if(regex.test(param)) {
+        serve.error(response, 416);
     } else {
         serve.error(response, 416);
     }
@@ -43,8 +43,10 @@ function getResult(response, param) {
     
     var onQuestionMark = param.split("?");
     var onEqualsSign = onQuestionMark[1].split("=");
-    var lineName = onEqualsSign[0];
-    var rating = parseInt(onEqualsSign[1]);
+    var onComma = onEqualsSign[1].split(",");
+    var userName = onComma[0];
+    var lineName = onComma[1];
+    var rating = parseInt(onComma[2], 10);
 
     switch(rating) {
         case 0 : 
@@ -93,6 +95,78 @@ function getResult(response, param) {
             serve.error(response, 416);
             break;
     }
+
+    var onError = function(error) {
+        if(error) {
+            serve.error(response, 416);
+        }
+    }
+    
+    db.userRatings.findOne({"userName" : userName},
+                            function(error, result) { 
+                                if(error) {
+                                    serve.error(response, 416);
+                                } else if(!result) {
+                                    var json = {
+                                        "userName" : userName, 
+                                        "Piccadilly" : null,
+                                        "District" : null,
+                                        "Victoria" : null,
+                                        "Circle" : null,
+                                        "Hammersmith_and_City" : null,
+                                        "Bakerloo" : null,
+                                        "Waterloo_and_City" : null, 
+                                        "Central" : null,
+                                        "Jubilee" : null,
+                                        "Metropolitan" : null,
+                                        "Northern" : null, 
+                                        "DLR" : null,
+                                        "Overground" : null
+                                    }
+                                    db.userRatings.insert(json, onError);
+                                }
+                            switch(lineName) { 
+                            case "Piccadilly" :
+                                db.update({"userName" : userName}, {$set : {"Piccadilly" : rating}}, onError);
+                                break;
+                            case "District" :
+                                db.update({"userName" : userName}, {$set : {"District" : rating}}, onError);
+                                break;
+                            case "Victoria" :
+                                db.update({"userName" : userName}, {$set : {"Victoria" : rating}}, onError);
+                                break;
+                            case "Circle" :
+                                db.update({"userName" : userName}, {$set : {"Circle" : rating}}, onError);
+                                break;
+                            case "Hammersmith_and_City" :
+                                db.update({"userName" : userName}, {$set : {"Hammersmith_and_City" : rating}}, onError);
+                                break;
+                            case "Bakerloo" :
+                                db.update({"userName" : userName}, {$set : {"Bakerloo" : rating}}, onError);
+                                break;
+                            case "Waterloo_and_City" :
+                                db.update({"userName" : userName}, {$set : {"Waterloo_and_City" : rating}}, onError);
+                                break;
+                            case "Jubilee" :
+                                db.update({"userName" : userName}, {$set : {"Jubilee" : rating}}, onError);
+                                break;
+                            case "Central" :
+                                db.update({"userName" : userName}, {$set : {"Central" : rating}}, onError);
+                                break;
+                            case "Metropolitan" :
+                                db.update({"userName" : userName}, {$set : {"Metropolitan" : rating}}, onError);
+                                break;
+                            case "Northern" :
+                                db.update({"userName" : userName}, {$set : {"Northern" : rating}}, onError);
+                                break;
+                            case "DLR" :
+                                db.update({"userName" : userName}, {$set : {"DLR" : rating}}, onError);
+                                break;
+                            case "Overground" :
+                                db.update({"userName" : userName}, {$set : {"Overground" : rating}}, onError);
+                                break;
+                            }                
+        });
 }
 
 /* Makes start method available to other modules */
